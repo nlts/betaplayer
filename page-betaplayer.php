fbef54e5ef93dc04cd720c330520c116a0b600dc
Matt Townsend <mtownsend@gatech.edu>
2015-04-27 23:00:44 -0400

    fix typo

--- page-betaplayer.php
+++ page-betaplayer.php
@@ -243,10 +243,10 @@ switch($stream) {
 	</div>
 	<script type="text/javascript">
 		jQuery( document ).ready(function() {
-			var a = jQuery('#play a')[0];
+			var a = jQuery('#play')[0];
 			var evObj = document.createEvent('MouseEvents');
 			evObj.initMouseEvent('click', true, true, window);
-			a.dispatchEvent(evObj);		
+			a.dispatchEvent(evObj);	
 		});
 	</script>
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/player.js"></script>
